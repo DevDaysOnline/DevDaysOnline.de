24bab026342b03f3606614c0e364774ca86943b3
Albert Weinert <info@der-albert.com>
2020-03-29 09:30:24 +0200

    added support text

---
+++ src/components/PayPal202004.vue
@@ -0,0 +1,40 @@
+<template>
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick" />
+        <input type="hidden" name="hosted_button_id" value="USE4SDPSG55WC" />
+        <table>
+          <tr>
+            <td>
+              <input type="hidden" name="on0" value="Unterstützung" />Unterstützung
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <select name="os0">
+                <option value="Zwanzig">Zwanzig €20,00 EUR</option>
+                <option value="Zehn">Zehn €10,00 EUR</option>
+                <option value="Fünf">Fünf €5,00 EUR</option>
+                <option value="Fünfzig">Fünfzig €50,00 EUR</option>
+                <option value="Fünfundsiebzig">Fünfundsiebzig €75,00 EUR</option>
+                <option value="Einhundert">Einhundert €100,00 EUR</option>
+              </select>
+            </td>
+          </tr>
+        </table>
+        <input type="hidden" name="currency_code" value="EUR" />
+        <input
+          type="image"
+          src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_paynowCC_LG.gif"
+          border="0"
+          name="submit"
+          alt="Jetzt einfach, schnell und sicher online bezahlen – mit PayPal."
+        />
+        <img
+          alt
+          border="0"
+          src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif"
+          width="1"
+          height="1"
+        />
+      </form>
+</template>
--- src/pages/Support.vue
+++ src/pages/Support.vue
@@ -1,25 +1,57 @@
 <template>
+  <Layout>
     <div>
-        <h2>Unterstützung</h2>
+      <h2>Unterstützen</h2>
+      <p>
+        Diese Ausgabe der dev-days-online wird unter anderem zur Unterstützung der Veranstalter der
+        Community Konferenzen
+        <a
+          href="https://www.dotnet-day-franken.de"
+        >.NET Day Franken 2020</a>,
+        der
+        <a href="https://dotnet-cologne.de">dotnet Cologne 2020</a>
+        den
+        <a href="https://md-devdays.de/">Magdeburger Developer Days 2020</a> veranstaltet.
+      </p>
+      <p>
+        Alle Konferenzen musste auf Grund von COVID-19 abgesagt, oder wenn möglich verschoben werden.
+        Jedoch sind schon in der Vorbereitung nicht unerhebliche Kosten angefallen, auf denen die Veranstalter mit dem schon
+        knappen Budget sitzenbleiben.
+      </p>
+      <p>
+        Du kannst helfen! Zahle freiwillig einen Betrag Deiner Wahl (ab 5 €), entweder bei Paypal oder per Überweisung.
+        Der dotnet Köln/Bonn e.V. sammelt alle Zahlungen ein, und die erlöse werden zu gleichen Teilen unter den Veranstaltern
+        ausgezahlt.
+        <br />Eine
+        <b>Rechnung</b> wird erst ab einem Betrag von
+        <b>mindestens 100 €</b> nach Aufforderung ausgestellt,
+        schicke dazu im Anschluss eine E-Mail mit den notwendigen Angaben an
+        <a href="mailto:info@dotnet-koelnbonn.de">info@dotnet-koelnbonn.de</a>
+      </p>
 
-     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="USE4SDPSG55WC">
-<table>
-<tr><td><input type="hidden" name="on0" value="Unterstützung">Unterstützung</td></tr><tr><td><select name="os0">
-	<option value="Zwanzig">Zwanzig €20,00 EUR</option>
-	<option value="Zehn">Zehn €10,00 EUR</option>
-	<option value="Fünf">Fünf €5,00 EUR</option>
-	<option value="Fünfzig">Fünfzig €50,00 EUR</option>
-	<option value="Fünfundsiebzig">Fünfundsiebzig €75,00 EUR</option>
-	<option value="Einhundert">Einhundert €100,00 EUR</option>
-</select> </td></tr>
-</table>
-<input type="hidden" name="currency_code" value="EUR">
-<input type="image" src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="Jetzt einfach, schnell und sicher online bezahlen – mit PayPal.">
-<img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
-</form>
+      <h3>PayPal</h3>
+      <p>
+        Wenn Du per PayPal unterstützen willst, so wähle hier einen Grund Betrag und auf der Folgeseite mit der Anzahl den
+        gewünschten Multiplikator.
+      </p>
 
-
+      <PayPal />
+      <h3>Überweisung</h3>
+      <p>Überweise den Betrag deiner Wahl an:</p>
+      <p>Kreissparkasse Troisdorf: <b>IBAN DE123408154711</b></p>
     </div>
+  </Layout>
 </template>
+
+
+<script>
+import PayPal from "~/components/PayPal202004.vue";
+export default {
+  components: {
+    PayPal
+  },
+  metaInfo: {
+    title: "Unterstützen"
+  }
+};
+</script>
